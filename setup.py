5240ac3487baf5ab43ec5f58e01d321571f3b380
Philipp Schlunder <philipp.schlunder@udo.edu>
2016-11-07 12:27:57 +0100

    Copyed setup from old repo

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python3
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Toolchain for analysing muon neutrinos within the energy \
+                    range of 1-1000 GeV with IceCube DeepCore.',
+    'author': 'Philipp Schlunder',
+    'url': 'https://github.com/WortPixel/phd_scripts',
+    'download_url': 'https://github.com/WortPixel/phd_scripts/archive/master.zip',
+    'author_email': 'philipp.schlunder@udo.edu',
+    'version': '0.1',
+    'install_requires': ['matplotlib',
+                         'numpy',
+                         'pandas',
+                         'paramiko',
+                         'scipy',
+                         'tables',
+                         'tqdm',
+                         ],
+    'packages': ['nuance',
+                 'nuance.data_handler',
+                 'nuance.icetray_modules',
+                 'nuance.job_handler'],
+    'scripts': [],
+    'name': 'nuance'
+}
+
+setup(**config)
