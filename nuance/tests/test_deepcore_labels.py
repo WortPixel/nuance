72296c4474da4012ba0f2797583777c6c1748a78
Philipp Schlunder <philipp.schlunder@udo.edu>
2016-11-21 17:26:49 +0100

    Working on test plots for deepcore labeling

--- nuance/tests/test_deepcore_labels.py
+++ nuance/tests/test_deepcore_labels.py
@@ -25,7 +25,8 @@ class TestDeepCoreLabels(unittest.TestCase):
         if not os.path.isdir(os.path.join(self.path, 'test')):
             os.mkdir(os.path.join(self.path, 'test'))
         self.i3file = os.path.expandvars('$THESIS/data/i3/cc_in_dc_test.i3.gz')
-        self.gcdfile = os.path.expandvars('$THESIS/data/i3/GeoCalibDetectorStatus_2013.56429_V1_Modified.i3.gz')
+        self.gcdfile = os.path.expandvars('$THESIS/data/i3/GeoCalibDetectorSta'\
+            'tus_2013.56429_V1_Modified.i3.gz')
         if not os.path.isfile(self.i3file):
             inp = input if sys.version_info[0] >= 3 else raw_input
             self.i3file = inp('Please provide path to an low E i3 file: ')
@@ -33,26 +34,23 @@ class TestDeepCoreLabels(unittest.TestCase):
             self.gcdfile = inp('Please provide matching gcd file: ')
 
 
-    #def test_DetectorByDoms(self):
-    #    pass
+    def tearDown(self):
+        run(['rm', '-rf', 'test'])
+        self.path = None
+        self.i3file = None
+        self.gcdfile = None
 
 
-    #def test_DetectorByContour(self):
-    #    pass
-
-
-    #def test_is_cc_in_detector(self):
-    #    pass
-
-
-    def test_general_attribute_insertion(self):
+class TestExistance(TestDeepCoreLabels):
+    def runTest(self):
         tray = I3Tray()
         tray.AddModule('I3Reader', 'reader', Filenamelist=[self.gcdfile,
                                                            self.i3file])
         tray.AddModule(dl.DeepCoreLabels,'labelmaker')
-        tray.AddModule('I3Writer', 'writer', Filename=os.path.join(self.path,
+        tray.AddModule('I3Writer', 'writer', Filename=os.path.join(
+            self.path,
             'test/ouput.i3.gz'), 
-                       Streams=[icetray.I3Frame.Physics,icetray.I3Frame.DAQ])
+            Streams=[icetray.I3Frame.Physics,icetray.I3Frame.DAQ])
         tray.AddModule('TrashCan', 'can')
         tray.Execute(10)
         del tray
@@ -64,8 +62,28 @@ class TestDeepCoreLabels(unittest.TestCase):
         del p_frame
 
 
-    def tearDown(self):
-        run(['rm', '-rf', 'test'])
+class CreateTestPlots(TestDeepCoreLabels):
+    def _get_data(i3file):
+        frames = []
+        # open i3 file
+        i3file = dataio.I3File(i3file)
+        # get interactions and positions of all frames (including all daughters)
+        pframe = i3file.pop_physics()
+        while pframe != None:
+            # CC, NC or other?
+            interactions = get_interaction_type(pframe)
+            positions = get_positions(pframe)
+            frames.append((positions, interactions))
+        return frames
+
+    def runTest(self):
+        self.data = self._get_data(self.i3file)
+        self._plot_top_view(self.data)
+
+
+
+
+
 
 
 if __name__ == '__main__':
